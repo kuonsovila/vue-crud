8f2ea930f7f1be464c857566cd3958b1028759fe
kuonsovila <kounsovila18@gmail.com>
2022-09-30 16:36:41 +0700

    first commit

---
+++ src/components/product/Create.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <HomeVue />
+    <div class="container">
+      <b-card>
+        <router-link to="/">
+          <i class="bi bi-arrow-left"></i>
+        </router-link>
+        <hr />
+        <b-form @submit.prevent="onSubmit">
+          <b-row>
+            <b-col cols="6">
+              <b-form-group id="input-group-1" label="Name" label-for="input-1">
+                <b-form-input
+                  v-model="name"
+                  type="text"
+                  placeholder="Khmer food etc."
+                  required
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col cols="6">
+              <b-form-group
+                id="input-group-1"
+                label="Price"
+                label-for="input-1"
+              >
+                <b-form-input
+                  id="input-1"
+                  type="number"
+                  v-model="price"
+                  placeholder="0.00 $"
+                  required
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="12">
+              <b-form-group
+                id="input-group-2"
+                label="Description"
+                label-for="input-2"
+              >
+                <b-form-input
+                  id="input-2"
+                  placeholder="description"
+                  v-model="description"
+                  required
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-button variant="primary" type="submit">Submit</b-button>
+        </b-form>
+      </b-card>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+// const baseUrl = "http://localhost:3001";
+import HomeVue from "../../views/Home.vue";
+export default {
+  components: {
+    HomeVue,
+  },
+  data() {
+    return {
+      name: "",
+      description: "",
+      price: 0,
+    };
+  },
+  methods: {
+    async onSubmit() {
+      // event.preventDefault();
+      await axios
+        .post("http://localhost:3001/products", {
+          name: this.name,
+          description: this.description,
+          price: this.price,
+        })
+        .then((res) => {
+          console.log("response", res.data);
+          this.$router.push("/");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    // onReset() {
+    //   this.name = "";
+    //   this.description = "";
+    //   this.price = 0;
+    // },
+  },
+};
+</script>
+<style>
+.container {
+  padding-top: 3%;
+  width: 60%;
+}
+.card {
+  padding: 10px;
+}
+</style>
---
+++ src/components/product/Product.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    <HomeVue />
+    <div class="form">
+      <b-row>
+        <!-- <h3>Products</h3> -->
+        <b-button variant="outline-warning" to="/product/new"
+          >New Product</b-button
+        >
+      </b-row>
+      <div class="form-data">
+        <table class="ttable">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <!-- <th>Type</th> -->
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="product in products" :key="product.id">
+              <!-- <td>{{ product.id }}</td> -->
+              <td>{{ product.name }}</td>
+              <td>{{ product.price }}</td>
+              <td>{{ product.description }}</td>
+              <td>
+                <button class="icon" @click="editProduct(product.id)">
+                  <i class="bi bi-pen"></i>
+                </button>
+                <button class="icon" @click="deleteDish(product.id)">
+                  <i class="bi bi-trash3"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const baseUrl = "http://localhost:3001";
+import HomeVue from "../../views/Home.vue";
+export default {
+  name: "Home",
+  components: {
+    HomeVue,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    this.getProducts();
+  },
+  methods: {
+    getProducts() {
+      axios.get(`${baseUrl}/products`).then((response) => {
+        console.log("products", response.data);
+        this.products = response.data;
+      });
+    },
+    editProduct(id) {
+      this.$router.push({ name: "updateProduct", params: { id: id } });
+    },
+    // getDisplayProduct(category) {
+    //   return {
+    //     id: category.id,
+    //     name: category.name,
+    //     imageUrl: category.imageUrl,
+    //     status: category.status,
+    //   };
+    // },
+  },
+};
+</script>
+<style scoped>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css");
+.form {
+  width: 80%;
+  margin: auto;
+  top: 10%;
+  padding: 20px;
+}
+table {
+  width: 100%;
+  margin: auto;
+}
+.ttable {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  border-radius: 7px 7px 0 0;
+  overflow: hidden;
+}
+.ttable thead tr {
+  /* padding: 15px; */
+  text-align: left;
+  background-color: aquamarine;
+}
+.ttable th,
+.ttable td {
+  padding: 15px;
+}
+
+.ttable tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+.icon {
+  border-radius: 5px;
+  /* background-color: bisque; */
+  border: none;
+  border: 1px solid rgb(223, 218, 218);
+  padding: 5px;
+  margin: 3px;
+}
+</style>
